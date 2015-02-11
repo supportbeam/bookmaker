d450483b4998d0b56562ffb29911bc031ecb72f3
Chris920 <cdtded1212@gmail.com>
2015-02-11 15:32:49 -0500

    Generated restaurant model, haven't migrated

---
+++ db/migrate/20150211203108_create_restaurants.rb
@@ -0,0 +1,14 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.text :description
+      t.string :address
+      t.string :phone_number
+      t.string :category
+      t.integer :capacity
+
+      t.timestamps null: false
+    end
+  end
+end
