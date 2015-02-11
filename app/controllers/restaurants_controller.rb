d82b83aca2221a5a6b21e0d341edffd520b56d00
Chris920 <cdtded1212@gmail.com>
2015-02-11 16:43:28 -0500

    finished restaurant MVC

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -25,8 +25,22 @@ class RestaurantsController < ApplicationController
   	else
   		render :new
   	end
-
   end
+
+	def update
+		@restaurant = Restaurant.find(params[:id])
+		if @restaurant.update_attributes(restaurant_params)
+			redirect_to restaurant_path(@restaurant)
+		else
+			render :edit
+		end
+	end
+	
+	def destroy
+		@restaurant = Restaurant.find(params[:id])
+		@restaurant.destroy
+		redirect_to restaurants_path
+	end
 
   private
   def restaurant_params
