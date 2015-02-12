c33fd960a459863174c37506f2422de98fd94d9d
JJLim <lim.jess9@gmail.com>
2015-02-12 15:03:51 -0500

    Edited available method in restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,7 @@ class Restaurant < ActiveRecord::Base
 
   def available?(party_size)
     # res_sum = reservations.all #all the reservations of the restaurant
-    total_res_count = reservations.where(party_time: party_time).sum(:party_size)
+    total_res_count = reservations.where(time: time).sum(:party_size)
     party_size + total_res_count <= capacity
   end
 end
