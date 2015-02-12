a0d89de650dc4f5f8d445f97c2bf46ffcb4cb1c0
Chris920 <cdtded1212@gmail.com>
2015-02-12 15:34:27 -0500

    Starting to work on Reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,32 @@
 class ReservationsController < ApplicationController
+	before_filter :load_restaurant
+
+	def new
+    @reservation = Reservation.new
+  end
+
+  def create
+  	@reservation = @restaurant.reservations.build(reservation_params)
+  	# @reservation.user = current_user
+		if @reservation.save
+  		redirect_to restaurant_path #(params[:restaurant_id])
+  	else
+  		render :new
+  	end
+  end
+
+	def destroy
+		@reservation = Reservation.find(params[:id])
+		@reservation.destroy
+		redirect_to restaurant_path
+
+	end
+
+ private
+  def reservation_params
+  	params.require(:reservation).permit(:party_size, :date, :time)
+  end
+  def load_restaurant
+  	@restaurant = Restaurant.find(params[:restaurant_id])
+  end
 end
